c870b92938c6a2958f6958f83c8ba3aa8ffaa873
Alex <19832622+AlexKraken@users.noreply.github.com>
2023-12-20 13:15:59 -0600

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -17,7 +17,19 @@ def create_app() -> Flask:
 
     from .models import Barcode
 
-    with app.app_context():
-        db.create_all()
+    #with app.app_context():
+    #    db.create_all()
 
+    # Check if the database needs to be initialized
+    engine = SQLAlchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+    inspector = SQLAlchemy.inspect(engine)
+    if not inspector.has_table("barcodes"):
+        with app.app_context():
+            db.drop_all()
+            db.create_all()
+            app.logger.info('Initialized the database!')
+    else:
+        app.logger.info('Database already contains the barcodes table.')
+
+    
     return app
