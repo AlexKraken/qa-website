1e80ca0bf2a81bfad21a692dc197693294d6f3f8
Alex <19832622+AlexKraken@users.noreply.github.com>
2023-12-20 19:56:01 -0600

    Add sample data to populate database

--- website/__init__.py
+++ website/__init__.py
@@ -18,19 +18,35 @@ def create_app() -> Flask:
 
     from .models import Barcode
 
-    #with app.app_context():
-    #    db.create_all()
-
     # Check if the database needs to be initialized
-    engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+    engine = sqlalchemy.create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
     inspector = sqlalchemy.inspect(engine)
     if not inspector.has_table("barcodes"):
         with app.app_context():
             db.drop_all()
             db.create_all()
-            app.logger.info('Initialized the database!')
-    else:
-        app.logger.info('Database already contains the barcodes table.')
 
-    
+            load_sample_data()  # Load sample data for demo purposes
+
+            app.logger.info("Initialized the database!")
+    else:
+        app.logger.info("Database already contains the barcodes table.")
+
     return app
+
+
+def load_sample_data():
+    # Load data from sample_data.csv into the Barcode model
+    from .models import Barcode
+
+    csv_file_path = os.path.join(os.path.dirname(__file__), "sample_data.csv")
+
+    with open(csv_file_path, "r") as csvfile:
+        header = csvfile.readline().strip().split(",")
+        for line in csvfile:
+            values = line.strip().split(",")
+            barcode_data = dict(zip(header, values))
+            barcode = Barcode(**barcode_data)
+            db.session.add(barcode)
+
+    db.session.commit()
